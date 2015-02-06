1a2404fa44062223cd3fe3b8f117bd3836d65964
Valeria Tiffenberg <vtiffenberg@manas.com.ar>
2015-02-06 11:05:35 +0700

    For #2 : First draft of get and post used in enquieing call

--- app/models/verboice.rb
+++ app/models/verboice.rb
@@ -1,24 +1,45 @@
 require 'net/http'
 
 class Verboice
-  attr_accessor :token
 
   def initialize(email, password)
     @email = email
     @password = password
+    connect
   end
 
-  def authenticate
-    auth_url = build_url("/auth")
-    response = Typhoeus.post(auth_url, body: { account: { email: @email, password: @password } })
-    self.token = JSON.parse(response.body)['auth_token']
-    response.success?
+  def enqueue_call(address, channel)
+    post("/call", { address: address, channel: channel })
+  end
+
+  def call_logs params = {}
+    verboice_query = auth_params(params.slice(:start_date, :end_date, :channel_id, :status)).to_query
+    get("/call_logs?#{verboice_query}")
   end
 
   private
 
+  def connect
+    auth_url = build_url("/auth")
+    response = Typhoeus.post(auth_url, body: { account: { email: @email, password: @password } })
+    @token = JSON.parse(response.body)['auth_token']
+  end
+
+  def get(path)
+    response = Typhoeus.get(build_url(path))
+    JSON.parse(response.response_body)
+  end
+
+  def post(path, params)
+    Typhoeus.post(build_url(path), body: auth_params(params))
+  end
+
   def build_url(path)
     ENV['VERBOICE_URL'] + path
+  end
+
+  def auth_params(params)
+    params.merge({ email: @email, token: @token })
   end
 
 end
