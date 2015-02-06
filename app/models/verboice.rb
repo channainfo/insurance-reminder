a925181e5fbf000d3c77de84768a4bdc424c2930
Valeria Tiffenberg <vtiffenberg@manas.com.ar>
2015-02-06 11:05:35 +0700

    For #2 : First draft of get and post used in enquieing call

--- app/models/verboice.rb
+++ app/models/verboice.rb
@@ -12,10 +12,31 @@ class Verboice
     auth_url = build_url("/auth")
     response = Typhoeus.post(auth_url, body: { account: { email: @email, password: @password } })
     self.token = JSON.parse(response.body)['auth_token']
+
     response.success?
   end
 
+  # def token
+  #   # pending defintion
+  # end
+
+  def enqueue_call(phone_number, channel)
+    post("/call", { address: phone_number, channel: channel })
+  end
+
   private
+
+  def get(path)
+    respone = Typhoeus.get(build_url(path))
+    JSON.parse(response.response_body)
+  end
+
+  def post(path, params)
+    with_auth_params = params.merge({ email: @email, auth_token: token })
+    p with_auth_params
+    response = Typhoeus.post(build_url(path), body: with_auth_params)
+    JSON.parse(response.response_body)
+  end
 
   def build_url(path)
     ENV['VERBOICE_URL'] + path
