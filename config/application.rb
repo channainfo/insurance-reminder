ba0280005c2f86fbbafbce422376d62907a8c3b6
Channa Ly <channa.info@gmail.com>
2015-02-25 09:26:59 +0700

    fix page title, app name, gitignore

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ Bundler.require(*Rails.groups)
 
 ENV.update YAML.load_file('config/application.yml')[Rails.env] rescue {}
 
-module RemindersAgent
+module InsurranceReminder
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
