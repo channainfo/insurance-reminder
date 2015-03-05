ed393b875afb61095f4ee9d307f98faf66d8123e
kakada <kakada.chheang@gmail.com>
2015-03-05 10:46:35 +0700

    set cron to update pending call status as error to every hours(fixes #28)

--- config/schedule.rb
+++ config/schedule.rb
@@ -9,8 +9,5 @@ set :output, "/tmp/insurance_reminder/cron_log.log"
 # see Capistrano roles section below
 every :hour, :roles => [:app] do
   rake "shpa:import_beneficiaries_expired"
-end
-
-every :day, :at => '12:01 am', :roles => [:app] do
   rake "shpa:update_delay_call_as_error"
 end
